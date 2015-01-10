5e8ed9c635c62ac0c76b99ab40e80dd7c0613732
David Rio Deiros <driodeiros@gmail.com>
2015-01-10 15:44:26 -0500

    Improve shell script for capturing image

--- scripts/take_image.sh
+++ scripts/take_image.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-imagesnap - > snapshot.jpg
-convert -resize 50% snapshot.jpg snapshot.png
-cat snapshot.png | uuencode -m - | sed '$d' | grep -v begin-base64 | tr '\n' ' '
-rm -f *.jpg *.png
+os=$(uname)
+
+if [ $os == "Darwin" ];then
+    imagesnap - > snapshot.jpg
+    convert -resize 50% snapshot.jpg snapshot.png
+    cat snapshot.png | uuencode -m - | sed '$d' | grep -v begin-base64 | tr '\n' ' '
+    rm -f *.jpg *.png
+else
+    exit 1 
+fi
